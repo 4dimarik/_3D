3120ae57b3dce7453448716ff2712238ddb00b62
Dritriy Golyshev <4dimariK@gmail.com>
2021-12-26 19:30:50 +0300

    устранение замечаний

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import Timer from "./modules/timer";
 
-const timer = new Timer(true);
+const timer = new Timer({
+  timerSelector: "#timer",
+  format: {
+    daysSelector: "#timer-days",
+    hoursSelector: "#timer-hours",
+    minutesSelector: "#timer-minutes",
+    secondsSelector: "#timer-seconds",
+  },
+  // separator: "name",
+});
 
 timer.start();
--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,23 +1,58 @@
-import { numeralsWithNouns } from "../utils";
-import { deadLine, nounsList } from "./config";
-
 export default class Timer {
-  constructor(withWords) {
-    this.withWords = true;
-    this.timer = document.querySelector("#timer");
-    this.timerDays = this.timer.querySelector("#timer-days");
-    this.timerHours = this.timer.querySelector("#timer-hours");
-    this.timerMinutes = this.timer.querySelector("#timer-minutes");
-    this.timerSeconds = this.timer.querySelector("#timer-seconds");
+  constructor({
+    deadLine = "2022-01-01",
+    timerSelector,
+    format = {},
+    separator = ":",
+  }) {
+    this.timerSelector = timerSelector;
+    this.formatSelectors = format;
+    this.separator = separator;
+    this.deadLine = deadLine;
+    this.init();
+  }
+  init() {
+    this.timer = document.querySelector(this.timerSelector);
+    this.format = {};
 
-    if (withWords) {
-      this.timerDaysAfter = this.timer.querySelector("#timer-days-after");
-      this.timerHoursAfter = this.timer.querySelector("#timer-hours-after");
-      this.timerMinutesAfter = this.timer.querySelector("#timer-minutes-after");
-      this.timerSecondsAfter = this.timer.querySelector("#timer-seconds-after");
+    if (this.timer) {
+      Object.keys(this.formatSelectors).forEach((selector) => {
+        let elementName = selector.replace("Selector", "");
+        this.format[elementName] = this.timer.querySelector(
+          this.formatSelectors[selector]
+        );
+      });
     }
   }
+  start() {
+    if (!this.isDateHasCome()) {
+      this.render();
+      this.updateClock = setInterval(this.render.bind(this), 1000);
+    }
+  }
+  stop() {
+    clearInterval(this.updateClock);
+  }
+  render() {
+    let dateStop = new Date(this.deadLine).getTime();
+    let dateNow = new Date().getTime();
+    if (this.isDateHasCome(this.deadLine)) {
+      this.stop();
+    } else {
+      const dateData = this.getTimeRemaining(dateStop, dateNow);
+      Object.keys(this.format).forEach((key) => {
+        this.format[key].textContent = this.getTwoDigitNumber(dateData[key]);
 
+        if (key !== "seconds") {
+          this.format[key].nextElementSibling.textContent = this.setSeparator(
+            this.format[key],
+            dateData[key],
+            key
+          );
+        }
+      });
+    }
+  }
   getTimeRemaining(dateStop, dateNow) {
     let timeRemaining = (dateStop - dateNow) / 1000;
 
@@ -36,49 +71,38 @@ export default class Timer {
   getTwoDigitNumber(num) {
     return String(num).length === 1 ? `0${num}` : `${num}`;
   }
-  isDayHasCome() {
-    return new Date(deadLine).getTime() <= new Date().getTime();
+  isDateHasCome() {
+    return new Date(this.deadLine).getTime() <= new Date().getTime();
   }
-  render() {
-    let dateStop = new Date(deadLine).getTime();
-    let dateNow = new Date().getTime();
-    if (this.isDayHasCome(deadLine)) {
-      this.stop();
-    } else {
-      const { days, hours, minutes, seconds } = this.getTimeRemaining(
-        dateStop,
-        dateNow
-      );
-      this.timerDays.textContent = this.getTwoDigitNumber(days);
-      this.timerHours.textContent = this.getTwoDigitNumber(hours);
-      this.timerMinutes.textContent = this.getTwoDigitNumber(minutes);
-      this.timerSeconds.textContent = this.getTwoDigitNumber(seconds);
-      if (this.withWords) {
-        this.timerDaysAfter.textContent = numeralsWithNouns(
-          days,
-          nounsList.day
-        );
-        this.timerHoursAfter.textContent = numeralsWithNouns(
-          hours,
-          nounsList.hour
-        );
-        this.timerMinutesAfter.textContent = numeralsWithNouns(
-          minutes,
-          nounsList.minute
-        );
-        this.timerSecondsAfter.textContent = numeralsWithNouns(
-          seconds,
-          nounsList.second
-        );
+  setSeparator(element, num = null, key = null) {
+    return this.separator !== "name"
+      ? this.separator
+      : this.getNameSeparator(num, key);
+  }
+  getNameSeparator(num, key) {
+    const nounsList = {
+      days: ["день", "дня", "дней"],
+      hours: ["час", "часа", "часов"],
+      minutes: ["минута", "минуты", "минут"],
+      seconds: ["секунда", "секунды", "секунд"],
+    };
+    const numeralsWithNouns = (num, nouns) => {
+      if (num === 1 || (num > 20 && +String(num)[1] === 1)) {
+        return nouns[0];
+      } else if (
+        (num > 1 && num < 5) ||
+        (num > 21 && +String(num)[1] > 1 && +String(num)[1] < 5)
+      ) {
+        return nouns[1];
+      } else if (
+        num === 0 ||
+        (num > 4 && num < 21) ||
+        (num > 21 && +String(num)[1] > 4) ||
+        +String(num)[1] === 0
+      ) {
+        return nouns[2];
       }
-    }
-  }
-  start() {
-    if (!this.isDayHasCome()) {
-      this.updateClock = setInterval(this.render.bind(this), 1000);
-    }
-  }
-  stop() {
-    clearInterval(this.updateClock);
+    };
+    return numeralsWithNouns(num, nounsList[key]);
   }
 }
