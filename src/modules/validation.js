599a5e77d4aedf0ae3fbd890adce4bb398ea28c1
Dritriy Golyshev <4dimariK@gmail.com>
2021-12-29 21:01:26 +0300

    end hard2

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,24 +1,52 @@
 export default class Validation {
   constructor() {
     this.setEventListener();
+    console.log("va");
   }
   setEventListener() {
     document.addEventListener("input", (e) => {
       this.validation(e);
     });
+    document.addEventListener(
+      "blur",
+      (e) => {
+        console.log(e);
+        this.validation(e);
+      },
+      true
+    );
   }
   calcValidation(target) {
     target.value = target.value.replace(/[^\d]/g, "");
   }
-  formFieldsValidation(target) {
+  formFieldsValidation(target, eventType) {
     const { name, value, type } = target;
     if (type === "text" || name === "user_message") {
-      target.value = value.replace(/[^а-я -]/gi, "");
+      if (eventType === "blur") {
+        target.value = this.blurValidation(value);
+      } else if (eventType === "input") {
+        target.value = this.inputValidation(value);
+      }
     } else if (type === "email") {
       target.value = value.replace(/[^a-z0-9@\-_.!~*']/gi, "");
     } else if (type === "tel") {
       target.value = value.replace(/[^0-9()\-]/g, "");
     }
+  }
+  inputValidation(value) {
+    return value.replace(/[^а-я -]/gi, "");
+  }
+  blurValidation(value) {
+    value = value
+      .replace(/[^а-я -]/gi, "")
+      .replace(/[ ]+/g, " ")
+      .replace(/[-]+/g, "-")
+      .replace(/^[- ]+|[ -]+$/g, "");
+    return [...value]
+      .map((char, index) =>
+        index === 0 ? char.toUpperCase() : char.toLowerCase()
+      )
+      .join("");
   }
   validation(e) {
     const { target } = e;
@@ -28,7 +56,7 @@ export default class Validation {
       this.calcValidation(target);
     }
     if (form) {
-      this.formFieldsValidation(target);
+      this.formFieldsValidation(target, e.type);
     }
   }
 }
